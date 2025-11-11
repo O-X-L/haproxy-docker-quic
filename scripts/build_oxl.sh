cf9ca8239c6bc1ba4bd2250299f8d239d73c787f
Rath Pascal <rath@oxl.at>
2025-11-12 00:32:51 +0100

    chore: remove test from build-script

--- scripts/build_oxl.sh
+++ scripts/build_oxl.sh
@@ -9,7 +9,9 @@ cd "$(dirname "$0")/../versions/${VERSION}"
 docker build -f Dockerfile_debian_openssl -t "oxlorg/haproxy:debian-quic-${VERSION}-openssl" --network=host --no-cache .
 docker build -f Dockerfile_debian_awslc -t "oxlorg/haproxy:debian-quic-${VERSION}-awslc" --network=host --no-cache .
 docker build -f Dockerfile_debian_awslc -t "oxlorg/haproxy:debian-quic-latest" --network=host .
+docker build -f Dockerfile_debian_awslc -t "oxlorg/haproxy:latest" --network=host .
 
 docker push "oxlorg/haproxy:debian-quic-${VERSION}-openssl"
 docker push "oxlorg/haproxy:debian-quic-${VERSION}-awslc"
 docker push "oxlorg/haproxy:debian-quic-latest"
+docker push "oxlorg/haproxy:latest"
