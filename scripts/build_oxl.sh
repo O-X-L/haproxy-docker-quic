d4a4538274a41c9c1b218904c9b497bccd9fe2e9
Rath Pascal <rath@oxl.at>
2026-01-08 21:16:46 +0100

    update CMD for more transparency

--- scripts/build_oxl.sh
+++ scripts/build_oxl.sh
@@ -10,8 +10,10 @@ docker build -f Dockerfile_debian_openssl -t "oxlorg/haproxy:${VERSION}-debian-q
 docker build -f Dockerfile_debian_awslc -t "oxlorg/haproxy:${VERSION}-debian-quic-awslc" --network=host --no-cache .
 docker build -f Dockerfile_debian_awslc -t "oxlorg/haproxy:debian-quic" --network=host .
 docker build -f Dockerfile_debian_awslc -t "oxlorg/haproxy:latest" --network=host .
+docker build -f Dockerfile_debian_awslc -t "oxlorg/waf-haproxy:latest" --network=host .
 
 docker push "oxlorg/haproxy:${VERSION}-debian-quic-openssl"
 docker push "oxlorg/haproxy:${VERSION}-debian-quic-awslc"
 docker push "oxlorg/haproxy:debian-quic"
 docker push "oxlorg/haproxy:latest"
+docker push "oxlorg/waf-haproxy:latest"
